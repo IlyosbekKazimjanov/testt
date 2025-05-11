c6487e20a330015eab1ef107b40c39596f83e58d
IlyosbekKazimjanov <ilyosbek.351.60.96>
2025-05-11 23:16:04 +0500

    main

---
+++ node.js
@@ -0,0 +1,126 @@
+//html-skalet qism
+//css-esa uning korinishi
+//javascript-esa uni harakatlanadigan qismi bolib hizmat qiladi
+
+//biz javasicripni shunchaki oynada emas consolda koramiz yokida terminalda korishimiz mumkin
+//html javascript yozib ketish uchun <script> codini ishlatamiz
+//compair-bu biz ishlatayotgan html css js larda compair tshunchasi bor
+//compair ozi nima degan savolga bu codlarni yurg`uzadi
+//
+//— JavaScript nima?-html skalet css dizayni JavaScript harakatlanishi
+//— Nodejs nima uchun kerak?-codlarni terminalda osonroq korish uchun
+//— JavaScript code ni necha hil usulda ishlatib(compile) korsak boladi
+//3 xil usulda compail qilsa boladi
+//in html
+//node console
+//online js compilers
+//
+//— JavaScript ishlash strukturasi haqida malumot bering?
+//— Single threaad nima?
+//— JavaScript code'ni necha hil usulda ishlatib(compile) ko'rsak
+//bo'ladi?
+//— Reserved keyword nima
+//— var let const larning bir biridan farqi nimada
+//— Global variable nima
+//
+//  tepadan pasga single threaad malti threaad dan iborat
+//  bitta paytda bitta ish bajarish
+//  3 xil usulda compail qilsa boladi
+//  band qilingan keyvordla
+//
+//    var eski versiya globa kerak bolmagan holatda xotiradan joy oladi
+//    let yangi versiya hotiradan kam joy oladi ecmaskript 6 da chiqan kerak bolmagan holatda xotiradan joy olmaydi
+//    const esa ozgarmas degani uni ozgartirib bolmaydi ecmastript 6 da chiqan
+//
+//  boshqa fayllarda ham ishlaydigan
+//biz htmlda linkni ulash uchun pastga <script src="./index.js"></script>
+//biz buni ishlatish uchun console.log(hello js)bunda biz terminal yoki consolda chiqarish mumkin
+//terminal ozi nima biz turgan manzilda nima borligi vaqtni kompyuterni ochirish yoqish kabi hamma vazifalarni qilsak boladi
+//ls - nima borligin korsatadi
+//.cd yozsak papkaga kirish
+//..cd papkadan chiqish
+//node v yozsak python versiya chiqaradi aynan java script ishlayotgan ishlamayotganini shu orqli bilsa boladi
+// singlesreg bir vaqtni ozida 1 ta ish bajaradi
+//
+
+//endi ozgaruvchi yaqratishni korib chiqamiz ozgaruvchini yaratish uchun hohlagan malumotimizni yozib olamiz faqat boshda _ yoki raqamda bolmasligi kerak
+//misol: true hell_o Hello1 false: _hello 3hello
+//biz ozgaruvchilarni let var cost orqali  beriladi
+//defold holatda var turadi
+//let yangi versiya
+
+//let yosh = 25
+// console.log(yosh)
+
+// malumotni yangilash uchun
+// let baho= 4;
+//baho = 5;
+//shu tarzda
+//var eski versiya
+// let num = 1
+// console.log(num)
+// huddi shu holatda yaratiladi
+//error turlari bor sintastic type error error turlar kop
+//stringlar ulari 3 xil turi bor `` "" ''
+// ""
+//''
+//``
+// let ism = "Zuhra"
+//console.log( "salom" + ism )
+//bularni o`zini ichida ishlatsa bolmadi
+// boolean bu 2 turga ajradi true false
+//true " " soz son o dan tashqari bular true
+//false esa qolgan bari
+// let talaba = true;
+
+//alert bor bu siz biror sahifada chiqadigan malumot
+// oddiy matn
+//alert("salom dunyo")
+
+//ozgaruvchi bilan esa
+//let ism ="Ali"
+//alert("salom" + ism)
+
+// prompt esa yozsa boladi
+// prompt("two ");
+//
+
+// let Hello1 = 4;
+// let hell_o = 4;
+// let hel1o = 4;
+
+// console.log(hel1o, Hello1, hell_o);
+// let ism = "Zuhra";
+// console.log("salom" + ism + "!");
+// console.log(Boolean(1));
+
+// = - teng
+// == - tengmi
+//  === - ham tipi ham valyusi
+// alert("salom dunyo");
+// console.log(alert(""));
+// console.log(prompt("salom meni yoshim 14 da senikichi  "));
+// x = "husanboy va asliddin dostlar";
+// console.log(prompt(x));
+// let ism = "husanboy";
+// let talaba = "asliddinga";
+// console.log("talabalar royxati:" + ism + talaba);
+
+// git va github
+
+// git bu biz yozgan prayektga saqlamiz turuvchi (versiya nazorati) desaham boladi
+// github esa yuklamiz va undan olsa boladi yani git bilan ishlaganda kodni saqlash uni boshqalarga berish/olish jamoa bilan ish olib borish mumkin
+
+// git init    --- projectni git ga ochib beradi
+// git confige user.name --- git confige fildagi nameni chiqaradi
+// git commit -m  "js lesson"    ---    # file ga izoh beradi
+// git config user.email  --- git confige fildagi emailni chiqaradi
+// git remote add origin link ---   githubdagi restory bilan local fileni boglab beradi
+// git push                # O‘zgarishlarni GitHubga yuboradi
+// git pull                # GitHubdan o‘zgarishlarni yuklab oladi
+// git clone                     # GitHub'dan loyihani yuklab olish
+// git branch                # Branchlar ro‘yxati
+// git checkout branch-nomi      # Branchga o‘tish
+//git add fayl nomi  --  faylni yuklaydi
+// repository bu bizni ishlarimizni saqlab togirlab beradi
+//https://github.com/IlyosbekKazimjanov/test.git
