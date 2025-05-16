6c449d9db96fdb12f02ab2bbbb55dcb7af8eed75
IlyosbekKazimjanov <ilyosbek.351.60.96>
2025-05-16 07:01:57 +0500

    main

---
+++ 2-dars.js
@@ -0,0 +1,18 @@
+// git va github
+
+// git bu biz yozgan prayektga saqlamiz turuvchi (versiya nazorati) desaham boladi
+// github esa yuklamiz va undan olsa boladi yani git bilan ishlaganda kodni saqlash uni boshqalarga berish/olish jamoa bilan ish olib borish mumkin
+
+// git init    --- projectni git ga ochib beradi
+// git confige user.name --- git confige fildagi nameni chiqaradi
+// git commit -m  "js lesson"    ---    # file ga izoh beradi
+// git config user.email  --- git confige fildagi emailni chiqaradi
+// git remote add origin link ---   githubdagi restory bilan local fileni boglab beradi
+// git push                # O‘zgarishlarni GitHubga yuboradi
+// git pull                # GitHubdan o‘zgarishlarni yuklab oladi
+// git clone                     # GitHub'dan loyihani yuklab olish
+// git branch                # Branchlar ro‘yxati
+// git checkout branch-nomi      # Branchga o‘tish
+//git add fayl nomi  --  faylni yuklaydi
+// repository bu bizni ishlarimizni saqlab togirlab beradi
+//https://github.com/IlyosbekKazimjanov/test.git
