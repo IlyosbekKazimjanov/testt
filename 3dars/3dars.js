1f2fdd53ae38f64074dde63cedff1558639c7f79
IlyosbekKazimjanov <ilyosbek.351.60.96>
2025-05-16 13:05:03 +0500

    main

---
+++ 3dars/3dars.js
@@ -0,0 +1,60 @@
+// type conversions
+// + qoshish
+// - ayrish
+// * kopaytrish
+// / bolish
+// ** daraja kotarish
+// % foizli boluv
+
+// stringdan numberga otkazish Number('12')
+// console.log(Number(19 * "2"));
+// console.log(Number("12" + "13"));
+// console.log(Number(12 + 13));
+// console.log(String(26));
+
+// a = 14;
+// console.log((a -= 3));
+// console.log((a += 3));
+// console.log((a /= 2));
+// console.log((a **= 3));
+// console.log((a *= 3));
+// console.log((a %= 3));
+
+// boolean - true yoki false
+// bularni bari false
+// console.log(Boolean(NaN));
+// console.log(Boolean(0));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(false));
+// console.log(Boolean(""));
+//bular esa true
+// console.log(Boolean(1));
+// console.log(Boolean(true));
+// console.log(Boolean("null"));
+// console.log(Boolean(" "));
+// console.log(Boolean("salom"));
+// parseInt - bu faqat songacha oladi
+// console.log(parseInt("26 young"));
+//taqqoslash operatorlari
+// console.log(Boolean(26 === 26));
+// console.log(Boolean(26 == 26));
+// console.log(Boolean(26 = 26));
+// console.log(Boolean(14 <= 26));
+// console.log(Boolean(81 >= 26));
+// toString bizga strigda chiqarib beradi faqat sanoq sistemada qilsa boladi
+// console.log((3).toString(10));
+// console.log(Number(true + false));
+// parseFloat faqat butun son oladi
+// console.log(parseFloat("2.13ilyos"));
+// console.log("apple " <= "banana");
+//and - &&  bu andni yozilishi
+// console.log(1 && 0);
+
+// or || trueni qidiradi
+// not ! teskarisiga aylantiradi
+// let a = 1; // true
+// let b = 0; // false
+// console.log(!(a || b));
+// let a = 2
+// console.log(!(1 || 0 && false ));
