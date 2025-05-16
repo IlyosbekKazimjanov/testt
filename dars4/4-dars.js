1524bf3d0ddbbcc02f427c813302f9b872730a6b
IlyosbekKazimjanov <ilyosbek.351.60.96>
2025-05-16 13:07:07 +0500

    main

---
+++ dars4/4-dars.js
@@ -0,0 +1,63 @@
+// if bu agar degani agar buyruq rost bolsa chiqadi
+//else aks holda degani  buyruq yolgon bolsa chiqadi
+// if (shart) { code
+//}  else{ code }
+// let age = 14;
+// if (age >= 18) {
+//   console.log("siz ulgaygansiz");
+// } else {
+//   console.log("sizni yoshingiz kichkina");
+// }
+// let tem = 30;
+// if (tem > 25) {
+//   console.log("havo issiq");
+// } else {
+//   console.log("havo sovuq");
+// }
+// let bal = 88;
+// if (bal >= 80) {
+//   console.log("tabriklayman siz imtihondan otingiz ");
+// } else {
+//   ("afsuski imtihondan ota olmadingiz");
+// }
+// let number = -5;
+// if (number >= 0) console.log("musbat son");
+// else {
+//   console.log("manfiy son");
+// }
+// ternery operatori
+// if = ?
+// else = :
+// sintac
+// shart ? code : code
+// let yosh = 27;
+// let shart = yosh >= 18 ? "siz ulgaygansiz" : "siz yoshsiz";
+// console.log(shart);
+// let ball = 50;
+// let shart = ball >= 50 ? "siz otingiz" : " afsustaman";
+// console.log(shart);
+// let hi = false;
+// let shart = hi ? 5 : 10;
+// console.log(shart);
+// let son = 0;
+// let tur = son >= 0 ? "musbat" : son < 0 ? "manfify" : " nol";
+// console.log(tur);
+// else if bir nechta shartlar bajarish uchun kerak
+
+// let baho = 4;
+// if (baho == 3) {
+//   console.log("yaxshi");
+// } else {
+//   console.log("yomon");
+// }
+// let baho = 0;
+
+// if (baho == 5) {
+//   console.log("Alo");
+// } else if (baho == 4) {
+//   console.log("qoniqarli");
+// } else if (baho == 3) {
+//   console.log("yaxshiro oqi ");
+// } else {
+//   console.log("O'qi");
+// }
